caabef11d7b505e9e3ed96a78bd404b7da5b5415
Justin Fay <justin.fay@hope.edu>
2023-04-20 08:25:44 -0400

    Added ability to output new string

--- SortString/windows32/example.asm
+++ SortString/windows32/example.asm
@@ -26,8 +26,10 @@ strBuff  BYTE    100 DUP (?)
 n        DWORD    ?
 minVar   BYTE    'z'
 minAdd   DWORD    ?
+
 tmp      DWORD    ?
 tmpAdd   DWORD    ?
+
 mem      DWORD    ?
 tmpChar  BYTE     ?
 
@@ -55,105 +57,30 @@ startLbl:
     mov n, ecx
     mov ecx, 0
 
-forOne: ; Find the minimum element in unsorted array
+forOne:
     cmp ecx, n
-    jge  doneLbl    ; if ecx < n jmp done
-    ;else find smallest element
-    mov bl, [ecx+eax]
-    cmp minVar, bl     ; if min > bl jmp new min
-    jg  newMin
-    jmp setJ        ; update j with i + 1
-    
-  
-forTwo:
-    cmp edi, n
-    jge swapLbl  ;if edi >= n jmp forOme
-    
-    mov bh, [eax+edi]   ; store the val of arr[j] in bh
-    mov tmp, eax        ; mov start of arr into tmp
-    mov al, minVar      ; mov minAdd into eax
-    cmp bh, al          ; compare arr[j] and arr[min]
-    mov eax, tmp        ; mov tmp back into eax
-
-    jl  upJ     
-    inc edi
-    jmp forTwo
-
-upJ:
-    mov esi, eax
-    add esi, edi
-    mov minAdd, esi
-   
-    mov esi, 0
-    inc edi
-    jmp forTwo
-
-swapLbl:
-    mov esi, minAdd
-    mov ebx, [esi]
-    mov tmp, ebx    ; succesfully stores arr[min] in temp
-
-    mov esi, eax    
-    add esi, ecx   
-
-    mov ebx, esi   ; ebx = i
-    mov eax, [ebx]
-    mov tmpChar, al ; tmpChar = array[i]
-
-    mov ebx, minAdd ; ebx = min element address
-    mov eax, 0
-    mov al, tmpChar
-    mov [ebx], eax  ; store arr[i] in arr[min]
-    ; Successfully stores arr[i] in arr[min]
-    
-    lea eax, inString
-    mov esi, eax    
-    add esi, ecx  ;esx = i
-
-    mov ebx, 0
-    mov ebx, tmp
-    mov [esi], bl 
-
-    
-    mov esi, 0
-    mov ebx, 0
+    jge doneLbl
     inc ecx
-    jmp forOne
-    
+    jmp loadBuff
 
-newMin:
-    mov minVar, bl      ; min character = arr[i]
 
-    mov esi, eax
-    add esi, ecx         ; min address = location in mem
+loadBuff:
+    lea edx, strBuff
+    mov eax, 0
+    mov ecx, 0
+    mov ebx, 0
+looper:
+    cmp ecx, n
+    jge doneLbl
+
+    mov  ebx, inString[ecx]
+    mov  al, bl
    
-    mov minAdd, esi     ; store in minAdd
-    mov esi, 0
-    ;inc ecx             ; inc   
-    jmp setJ          ; restart
-    
-setJ:
-    mov edi, ecx    ; set edi = j = i (ecx)
-    inc edi         ; i+1 (edi++)
-    jmp forTwo      ;Start innerLoop
-
-
+    mov strBuff[ecx], al 
+    inc ecx
+    jmp looper
 
 doneLbl:
-       mov ecx, 0
-       lea eax, inString
-       lea ebx, strBuff
-
-whileLbl:
-    cmp ecx, n
-    jge helpLbl
-    mov esi, [eax+ecx]
-    dtoa edx, esi
-    mov [ebx+ecx], edx
-    jmp whileLbl
-
-    
-helpLbl:
 ;***  Print the new sorted list
         output  strBuff, strBuff
         mov     eax, 0  ; exit with return code 0
