cffffe1a8c60e4d47747d58b961818ff94ec74f1
JustinFay01 <JustinFay@hope.edu>
2023-04-19 23:36:26 -0400

    Starting Insertion Sort on string

---
+++ SortString/windows32/example.asm
@@ -0,0 +1,69 @@
+;  Week 14 day 1 Exercise: Bubble Sort!
+;
+;  Mike Jipping
+;  April 2019
+
+
+.586
+.MODEL FLAT
+
+INCLUDE io.h            ; header file for input/output
+
+.STACK 4096
+
+.DATA
+
+temp    DWORD   ?
+string  BYTE    100 DUP (?)
+numstr  BYTE    11 DUP (?), 0
+prompt1 BYTE    "Unsorted List", 0
+prompt2 BYTE    "Sorted List", 0
+inlimit DWORD   ?
+
+inString DWORD   100 DUP (?)
+inWrd    BYTE    "Enter a word you would like to sort", 0
+strBuff  BYTE    100 DUP (?)
+.CODE
+_MainProc PROC
+
+;*************** Insertion Sort Code ************************
+        input   inWrd, inString, 100
+
+;***  Print the new sorted list
+
+display2:
+        ; ECX holds the address of the string being built
+        lea	   ecx, string
+
+        ; EBX will hold the address of the current item in the array
+        lea    ebx, array
+
+        ; Look for the 0 at the end of the list
+out2:   mov    eax, [ebx]
+        cmp    al, 0
+        je     outputstring2
+
+        ; Convert the number to a series of characters
+        dtoa   numstr, eax
+
+        ; Copy the characters to the string being build
+        lea    edx, numstr
+while2: mov     eax, [edx]
+        cmp     al, 0
+        jne     movechar2
+        add     ebx,4
+        jmp     out2
+movechar2:
+        mov     [ecx], eax      ; Move the character
+        inc     edx             ; Increment counters
+        inc     ecx
+        jmp     while2
+
+        ; Now output the string.
+outputstring2:
+        output  prompt2, string
+
+        mov     eax, 0  ; exit with return code 0
+        ret
+_MainProc ENDP
+END                             ; end of source code
