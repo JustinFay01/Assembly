9c062d5b4874a5fab3df94747582ade6877684d5
JustinFay01 <JustinFay@hope.edu>
2023-04-20 22:54:07 -0400

    First test passed

--- FormatString/windows32/example.asm
+++ FormatString/windows32/example.asm
@@ -8,7 +8,7 @@ INCLUDE io.h            ; header file for input/output
 .DATA
 
 sentence    BYTE        100 DUP (?)
-strBuff     DWORD       ?
+strBuff     BYTE        100 DUP (?)
 inPrompt    BYTE        "Enter a sentence you would like to format", 0
 n           DWORD       ?
 
@@ -32,13 +32,16 @@ startLbl:
     mov n, ecx
     mov ecx, 0
     mov ebx, 0
+    lea esi, strBuff
 
 forLoop:
     cmp ecx, n
     jge doneLbl
 
-    push strBuff   ;eax = strBuff             ; push param2
-    push ebx       ;ebx = character to check  ; push param1
+    mov bl, sentence[ecx]
+
+    push esi       ;esi = strBuff location in mem   ; push param2
+    push ebx       ;ebx = character to check        ; push param1
     call fctn1     ;returns formatted char already in strBuff
     
     inc ecx
@@ -51,25 +54,52 @@ doneLbl:
         ret
 _MainProc ENDP
 
+
+; Func needs to check if string is upper or lower case
+; Change accordingly 
+; return the change in the correct indec of strBuff
+
 fctn1   PROC
         push    ebp             ; save base pointer
         mov     ebp, esp        ; establish stack frame
-        push    ebx             ; save EBX
+        lea     eax, strBuff
+        
+        ; first check ecx
+        cmp ecx, 0
+        jz  chkLowera   ; if 0 jmp to check if lower case
 
+        ; else ; chk upperA (CHARACTER STORED IN BL(EBX))
+        cmp bl, 'A' ; if bl >= A
+        jge chkUpperZ  ; jmp to check upperZ
+        jmp addBuff    ; else jmp to just add it (special char)
+        
+              
 
+chkLowera: ; checks if bl >= 'a'
+    cmp bl, 'a'
+    jge chkLowerz
+    jmp addBuff
 
+chkLowerz:  ; checks if bl <= 'z', if so change bl to UpperCase and jmp add bl to Buff
+    cmp bl, 'z' 
+    jg addBuff ;if bl > z then its not a letter and move on
 
+    ;else move to upper case
+     sub bl, 32 
+     jmp addBuff
 
+chkUpperZ: ; checks if n; <= 'Z', if so change bl toLowerCase nad jmp add bl to Buff
+    cmp bl, 'Z'
+    jg addBuff
 
+    add bl, 32
+    jmp addBuff
 
+addBuff:    ; add bl to buff 
+    mov  [esi + ecx], bl
 
-
-        pop     ebx             ; restore EBX
         pop     ebp             ; restore EBP
         ret                     ; return   
-        
-
-
 
 fctn1   ENDP
 
