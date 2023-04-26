0feab8e0bf6ae886170f5328d3044aed48a18af1
JustinFay01 <JustinFay@hope.edu>
2023-04-25 23:25:08 -0400

    Can recursivley detect pal, using weird pop duplication

--- MP8/windows32/example.asm
+++ MP8/windows32/example.asm
@@ -13,7 +13,8 @@ INCLUDE io.h            ; header file for input/output
 
 prompt          BYTE    "Enter phrase", 0
 string          BYTE    40 DUP (?)
-resulta BYTE    10 DUP (?)
+resulta         BYTE    10 DUP (?)
+strLen          DWORD   ?
 
 .CODE
 _MainProc PROC
@@ -51,13 +52,21 @@ incLbl:
 ; /////////////////////// CAPITAL IS NOW LOWERCASE ////////////////////////
 
 COMPARE: ; ******** Call Recursive Fun ***********
-    
+;  Now we have the length of the string.  Let's analyze the front 
+;  of the string and the end of the string.
 
+        mov     strLen, ecx
+        dec     strLen            ; set strLen to be one less than the null terminatior        
 
+        ; *******Start recursive call*********
+        mov     eax, 0
+        push    eax             ; push i
+        mov     eax, strLen     
+        push    eax             ; push j (length of arr - 1)
+        lea     eax, string
+        push    eax             ; push starting address
 
-
-
-
+        call isPal
         ; Return: adjust the stack (throw away the parameters), print, quit.
         add     esp, 12     ; discard two of the parameters  
 
@@ -72,46 +81,69 @@ quit:   mov     eax, 0      ; exit with return code 0
 _MainProc ENDP
 
 isPal	PROC
-
-; FILL IN CODE HERE!!!
         push    ebp             ; save base pointer
         mov     ebp, esp        ; establish stack frame
-        push    ebx             ; i 
-        push    ecx             ; j (length of array - 1)
+        push    edi             ; i 
+        push    esi             ; j (length of array - 1)
         push    edx             ; location of array
+      
+        ; local variables for comparison
+        push   ebx              ; Represent arr[i]
 
-        mov     ebx, dword ptr [ebp + 16] ; --> ebp + 16 is the First Param which is the index
+        mov     edi, dword ptr [ebp + 16] ; --> ebp + 16 is the First Param which is the index
+        mov     esi, dword ptr [ebp + 12] ; --> ebp + 12 is the Second Param which is the len of arr
         mov     edx, dword ptr [ebp + 8]  ; --> ebp + 8 gets third param which is closest and passed the return val
-        mov     ecx, dword ptr [ebp + 12] ; --> ebp + 12 is the Second Param which is the len of arr
 
-        cmp     ebx, ecx
-        jge     doneLbl
-        
-        ; RECURSIVE CALL
-        inc     ebx                     ; inc ebx
-        mov     eax, [edx + ebx*4]      ; load new val of array
-        push    ebx
-        push    ecx
-        push    edx
-        call isPal
+        cmp     edi, esi        ; if i > j done
+        jg      doneLbl
 
-        ; Clear params for recursive call
-        add esp, 12 
-        cmp [edx + ebx*4], eax
-        jl  retCurr
-        jmp doneLbl
 
-retCurr:
-        mov eax, [edx + ebx*4]
-        jmp doneLbl
-    
+        mov    bl, [edx+edi]
+        cmp    bl, [edx+esi]
+        jnz    notPal           ; if arr[i] != arr[j] return 0
+
+       ; else make recursive call
+       inc edi  ; i++
+       dec esi  ; j--
+
+       pop ebx  ; pop local variable which is the current char
+
+       push edi ; push i
+       push esi ; push j
+       push edx ; push local in mem of arr
+
+       call isPal  ; recursive call
+       add esp, 12 ; clear params
+
+
+       pop     edx
+       pop     esi
+       pop     ebx
+       pop     ebp 
+
+       ret
 
 doneLbl:
-        pop     edx
-        pop     ecx
-        pop     ebx
-        pop     ebp             ; restore EBP
-        ret                      ; return      
+       pop     ebx
+       pop     edx
+       pop     esi
+       pop     ebx
+       pop     ebp 
+
+       mov eax, 1      ; return 1
+       ret
+		
+
+notPal:
+        
+       pop     ebx
+       pop     edx
+       pop     esi
+       pop     ebx
+       pop     ebp 
+
+       mov eax, 0       ; return 0
+       ret
 		
 isPal ENDP
 
